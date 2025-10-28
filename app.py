8fc64a11947908dd841bc01d73cc471aa8fdb479
rayoen0 <rayane.el.metni@gmail.com>
2025-10-28 14:16:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,59 @@
-#%%writefile app.py
+# Import libraries
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title="Medals visualisation",layout="wide")
-st.title("Medals visualisation")
+st.set_page_config(page_title="Medals Visualization", layout="wide")
+st.title("Medals Visualization")
 
-#Dropdown menu
-medal = st.selectbox("Select a medal type",["Gold","Silver","Bronze"])
+# Dropdown menu
+medal = st.selectbox("Medal type", ["gold",'silver','bronze'])
 
-#checkboxes
-st.checkbox("Show Bar Chart", value = True)
-st.checkbox("Show Pie Chart", value = True)
+# checkboxes
 
-#two-col structure
+show_bar = st.checkbox("Show Bar Chart",value=True)
+show_pie = st.checkbox("Show Pie Chart",value=True)
+
+# two-col structure
 
 col1, col2 = st.columns(2)
 
+# load the medal wide dataset
 df = px.data.medals_wide()
 
+# plot the bar chart
 
-#plot bar chart
 if show_bar:
   fig_bar = px.bar(
       df,
-      x = "nation",
-      y=f"{medal}"
+      x="nation",
+      y=f"{medal}",
+      title = f"Medals count ({medal})"
   )
+
   fig_bar.update_layout(
       title_x=0.5,
       xaxis_title="Country",
-      yaxis_title="Count"
-      width = 300, height = 300
-      yaxis_title=f"{medal}"
+      yaxis_title="Count",
+      height = 300
   )
+
   col1.plotly_chart(fig_bar, use_container_width=True)
 
-if show pie:
+
+if show_pie:
   fig_pie = px.pie(
-    df,
-    names= "Country",
-    values=f" [medal)",
-    title = f"Medals count ((medal))"
+      df,
+      names="Country",
+      values=f"{medal}",
+      title = f"Medals count ({medal})"
   )
+
   fig_pie.update_layout(
-    title_x=8.5,
-    height = 300)
-co12. plotly_chart(fig_pie, use_container width=True)
+      title_x = 0.5,
+      height = 300
+  )
+
+  col2.plotly_chart(fig_pie, use_container_width=True)
+  
